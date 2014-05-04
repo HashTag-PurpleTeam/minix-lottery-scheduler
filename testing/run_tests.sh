b61875deca4a4192888a58db4c872bec8b033c6a
Leland <lelandmiller@gmail.com>
2014-05-04 00:38:08 +0000

    Last testing commit.

--- testing/run_tests.sh
+++ testing/run_tests.sh
@@ -38,7 +38,7 @@ echo "Generates random data files and runs the IO tests on them."
 
 for i in 1 2 3 4
 do
-	dd if=/dev/random of=tmp/t$i.data bs=1048576 count=25 2>/dev/null
+	dd if=/dev/random of=tmp/t$i.data bs=1048576 count=10 2>/dev/null
 done
 
 for i in 1 2 3
@@ -65,8 +65,10 @@ echo "## Stage 3"
 echo
 echo "Process 2 runs with half the tickets of process 1."
 
-(time ./cpu_sim_quiet 10) >tmp/1.out 2>tmp/1.err 
+(time ./cpu_sim_quiet 10&) >tmp/1.out 2>tmp/1.err 
 (time nice -n -10 ./cpu_sim_quiet 10) >tmp/2.out 2>tmp/2.err 
+
+sleep 2
 
 for i in 1 2
 do
@@ -76,6 +78,26 @@ do
 	cat tmp/$i.err
 done
 
+# STAGE 4 
+
+echo
+echo "## Stage 4"
+echo
+echo "Process 1 has 1 ticket, process 2 has 20, and process 3 has 40 tickets."
+
+(time nice -n -19 ./cpu_sim_quiet 10&) >tmp/1.out 2>tmp/1.err 
+(time ./cpu_sim_quiet 10&) >tmp/2.out 2>tmp/2.err 
+(time nice -n 20 ./cpu_sim_quiet 10) >tmp/3.out 2>tmp/3.err 
+
+sleep 2
+
+for i in 1 2 3
+do
+	echo
+	echo "Process $i"
+	cat tmp/$i.out
+	cat tmp/$i.err
+done
 
 rm -r tmp
 
